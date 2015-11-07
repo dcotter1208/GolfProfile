40bc6a14ba1706404e3e7e4e35c721bf026e08ce
Donovan Cotter <donovan.cotter@yahoo.com>
2015-11-06 23:42:17 -0500

    friends are now displayinf on the friends view controller

---
+++ GolfProfile/FriendsCell.swift
@@ -0,0 +1,25 @@
+//
+//  FriendsCell.swift
+//  GolfProfile
+//
+//  Created by Donovan Cotter on 11/6/15.
+//  Copyright © 2015 DonovanCotter. All rights reserved.
+//
+
+import UIKit
+
+class FriendsCell: UITableViewCell {
+    @IBOutlet weak var friendUserNameCellLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
