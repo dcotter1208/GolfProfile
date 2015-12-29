8748a673ede384d4bbf8f279cecbbb2656e4e94e
Donovan Cotter <donovan.cotter@yahoo.com>
2015-12-29 18:35:39 -0500

    json data now written to Realm Browser

---
+++ GolfProfile/Course.swift
@@ -0,0 +1,16 @@
+//
+//  Course.swift
+//  GolfProfile
+//
+//  Created by Donovan Cotter on 12/29/15.
+//  Copyright © 2015 DonovanCotter. All rights reserved.
+//
+
+import Foundation
+import Realm
+
+class Course: RLMObject {
+    dynamic var name = ""
+    dynamic var city = ""
+    dynamic var state = ""
+}
--- GolfProfile/GolfCourseInfoAPI.swift
+++
@@ -1,9 +0,0 @@
-//
-//  GolfCourseInfoAPI.swift
-//  GolfProfile
-//
-//  Created by Donovan Cotter on 12/10/15.
-//  Copyright © 2015 DonovanCotter. All rights reserved.
-//
-
-import Foundation
