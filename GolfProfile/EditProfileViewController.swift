9908ff0e52dafc12e32ffff66e4d63c7b6a87ca5
Donovan Cotter <donovan.cotter@yahoo.com>
2016-01-04 00:28:34 -0500

    editProfileVC constraints set and background color changes.

--- GolfProfile/EditProfileViewController.swift
+++ GolfProfile/EditProfileViewController.swift
@@ -33,6 +33,10 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
         
     }
     
+    override func viewWillAppear(animated: Bool) {
+        loadUserProfile()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
